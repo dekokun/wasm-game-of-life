8c8af3046816db0b31085faa0e535b9a77695299
dekokun <shintaro.kurachi@gmail.com>
2018-11-22 22:34:16 +0900

    共通処理をくくりだす

--- src/lib.rs
+++ src/lib.rs
@@ -131,8 +131,9 @@ impl Universe {
   fn get_index(&self, row: u32, column: u32) ->usize {
     (row * self.width + column) as usize
   }
-  fn live_neighbor_count(&self, row: u32, column: u32) -> u8 {
-    let mut count = 0;
+  fn neighbor_indexs(&self, row: u32, column: u32) -> [usize; 8] {
+    let mut neighbors = [0; 8];
+    let mut neighbor_index = 0;
     for delta_row in [self.height - 1, 0, 1].iter().cloned() {
       for delta_col in [self.width - 1, 0, 1].iter().cloned() {
         if delta_row == 0 && delta_col == 0 {
@@ -141,8 +142,16 @@ impl Universe {
         let neighbor_row = (row + delta_row) % self.height;
         let neighbor_col = (column + delta_col) % self.width;
         let idx = self.get_index(neighbor_row, neighbor_col);
-        count += self.cells[idx] as u8;
+        neighbors[neighbor_index] = idx;
+        neighbor_index += 1;
       }
+    }
+    neighbors
+  }
+  fn live_neighbor_count(&self, row: u32, column: u32) -> u8 {
+    let mut count = 0;
+    for idx in &self.neighbor_indexs(row, column) {
+        count += self.cells[*idx] as u8;
     }
     count
   }
